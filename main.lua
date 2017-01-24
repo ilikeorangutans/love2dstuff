f9d8549636156f41ea3e773bb3de93c60de0549a
Jakob Külzer <jakob.kulzer@shopify.com>
2017-01-24 16:14:57 -0500

    Basic view state handling for colonies.

--- main.lua
+++ main.lua
@@ -3,6 +3,7 @@ stringx.import()
 local tablex = require 'pl.tablex'
 
 require 'util'
+require 'viewstate'
 require 'predicate'
 require 'bus'
 require 'tileset'
@@ -137,54 +138,29 @@ function love.load()
 
   viewStack = ViewStack
   table.insert(viewStack, gameMapView)
+
+  gameViewStateHandler = GameViewStateHandler
+  bus:subscribe("selection.selected", gameViewStateHandler, GameViewStateHandler.onSelectEntity)
+end
+
+GameViewStateHandler = {}
+
+function GameViewStateHandler:onSelectEntity(e)
+  local comps = entityManager:get(e.id)
+
+  if comps.colony then
+    print("COLONY, CHANGING VIEW STATE")
+    local colonyView = ColonyView
+    table.insert(viewStack, colonyView)
+  end
 end
 
 function love.resize(w, h)
   viewport:resize(w, h)
 end
 
-ViewStack = {}
-
-GameMapView = {
-  viewport = {},
-  mapView = {}
-}
-
-function GameMapView:draw()
-  local mapView = self.mapView
-  local viewport = self.viewport
-  viewport:draw()
-
-  local tile = mapView:getAt(mousePosition)
-  local explored = mapView:isExplored(mousePosition)
-  local tileMap = map:getAt(mousePosition)
-  love.graphics.setColor(0, 0, 0)
-  love.graphics.rectangle('fill', 600, 520, 200, 50)
-  love.graphics.setColor(255, 255, 255)
-  love.graphics.print(("Mouse: %d/%d"):format(mousePosition.x, mousePosition.y), 600, 520)
-  love.graphics.print(("%s"):format(tile.terrain.title, explored), 600, 535)
-  love.graphics.print(("Turn: %d Player: %s"):format(game.turn + 1, game:currentPlayer().name), 600, 550)
-  if selectionManager.selected then
-    local comps = entityManager:get(selectionManager.selected)
-    if comps.action then
-      local current = "-no orders-"
-      if comps.action.current then
-        current = comps.action.current.action
-      end
-      love.graphics.print(("%d/%d %s"):format(comps.action.points.left, comps.action.points.max, current), 600, 580)
-    end
-
-    local desc = ""
-    if comps.colonist then
-      desc = comps.colonist.profession.title
-    end
-
-    love.graphics.print(("[%d] %s, owner: %s"):format(selectionManager.selected, desc, comps.owner.id), 600, 565)
-  end
-end
-
 function love.draw()
-  local viewState = viewStack[1]
+  local viewState = viewStack[#(viewStack)]
   viewState:draw()
 end
 
---
+++ viewstate.lua
@@ -0,0 +1,52 @@
+-- This thing needs stack methods.
+ViewStack = {}
+
+
+ColonyView = {}
+function ColonyView:draw()
+  love.graphics.setColor(80, 80, 80)
+  love.graphics.rectangle('fill', 600, 520, 200, 50)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.print("COLONY")
+end
+
+-- map view of the colonies
+GameMapView = {
+  viewport = {},
+  mapView = {}
+}
+
+function GameMapView:draw()
+  local mapView = self.mapView
+  local viewport = self.viewport
+  viewport:draw()
+
+  local tile = mapView:getAt(mousePosition)
+  local explored = mapView:isExplored(mousePosition)
+  local tileMap = map:getAt(mousePosition)
+  love.graphics.setColor(0, 0, 0)
+  love.graphics.rectangle('fill', 600, 520, 200, 50)
+  love.graphics.setColor(255, 255, 255)
+  love.graphics.print(("Mouse: %d/%d"):format(mousePosition.x, mousePosition.y), 600, 520)
+  love.graphics.print(("%s"):format(tile.terrain.title, explored), 600, 535)
+  love.graphics.print(("Turn: %d Player: %s"):format(game.turn + 1, game:currentPlayer().name), 600, 550)
+  if selectionManager.selected then
+    local comps = entityManager:get(selectionManager.selected)
+    if comps.action then
+      local current = "-no orders-"
+      if comps.action.current then
+        current = comps.action.current.action
+      end
+      love.graphics.print(("%d/%d %s"):format(comps.action.points.left, comps.action.points.max, current), 600, 580)
+    end
+
+    local desc = ""
+    if comps.colonist then
+      desc = comps.colonist.profession.title
+    end
+
+    love.graphics.print(("[%d] %s, owner: %s"):format(selectionManager.selected, desc, comps.owner.id), 600, 565)
+  end
+end
+
+
